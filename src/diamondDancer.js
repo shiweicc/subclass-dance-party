32dd8d4a2cb405cf62ca93c5e7cc1dc852775da7
Shiwei Chen <shiweichenus@gmail.com>
2022-03-18 23:51:57 -0500

    Complete adding css

--- src/diamondDancer.js
+++ src/diamondDancer.js
@@ -20,7 +20,7 @@ DiamondDancer.prototype.step = function() {
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.toggle();
+  // this.$node.toggle();
 };
 
 // BlinkyDancer.prototype.changeColor = function() {
